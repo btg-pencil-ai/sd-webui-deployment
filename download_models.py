3725f931079957df96e33ba89e34744cc77e808b
Adithya-Shenoi <adithya.s@keyvalue.systems>
2023-09-27 18:12:45 +0530

    Updated the python script to download models

--- download_models.py
+++ download_models.py
@@ -1,9 +1,59 @@
 import os
-import sys
+from logging import getLogger
+from huggingface_hub import hf_hub_download
+
 
 MAIN_MODELS_PATH=os.environ.get("MAIN_MODELS_PATH", 
                                 "/stable-diffusion-webui/models")
 CONTROLNET_EXTENSION_MODELS_PATH=os.environ.get("CONTROLNET_EXTENSION_MODELS_PATH", 
                                                 "/stable-diffusion-webui/extensions/sd-webui-controlnet/models")
 
-raise NotImplementedError("Need to implement this")
+logger = getLogger(__name__)
+
+class HuggingFaceModelDownloader():
+    def __init__(self) -> None:
+        logger.info("Starting Hugging Face Model Downloader")
+
+        self.ensure_directory_exists(MAIN_MODELS_PATH)
+        self.ensure_directory_exists(CONTROLNET_EXTENSION_MODELS_PATH)
+
+        models_to_download = [
+            {'repo_id': 'stabilityai/stable-diffusion-xl-base-1.0', 'filename': 'sd_xl_base_1.0.safetensors', 'filepath': MAIN_MODELS_PATH},
+            {'repo_id': 'stabilityai/stable-diffusion-xl-refiner-1.0', 'filename': 'sd_xl_refiner_1.0.safetensors', 'filepath': MAIN_MODELS_PATH},
+            {'repo_id': 'stabilityai/sdxl-vae', 'filename': 'sdxl_vae.safetensors', 'filepath': MAIN_MODELS_PATH},
+            {'repo_id': 'stabilityai/sd-vae-ft-mse-original', 'filename': 'vae-ft-mse-840000-ema-pruned.ckpt', 'filepath': MAIN_MODELS_PATH},
+            {'repo_id': 'lllyasviel/sd_control_collection', 'filename': 'diffusers_xl_canny_full.safetensors', 'filepath': CONTROLNET_EXTENSION_MODELS_PATH},
+            {'repo_id': 'lllyasviel/sd_control_collection', 'filename': 'sai_xl_depth_256lora.safetensors', 'filepath': CONTROLNET_EXTENSION_MODELS_PATH},
+        ]
+        
+        for model in models_to_download:
+            self.download_model(model)
+
+    
+    def download_model(self, model):
+        repo_id = model['repo_id']
+        filename = model['filename']
+        target_path = os.path.join(model['filepath'], filename)
+        
+        # Check if the file already exists in the local directory
+        if not os.path.exists(target_path):
+            try:
+                hf_hub_download(
+                    repo_id=repo_id,
+                    filename=filename,
+                    local_dir=model['filepath']
+                )
+                logger.info(f"{filename} downloaded successfully")
+            except Exception as e:
+                logger.error(f"Failed to download {filename}: {e}")
+        else:
+            logger.info(f"{filename} already exists in the local directory, skipping download.")
+
+    def ensure_directory_exists(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            logger.info(f"Directory '{directory}' created successfully.")
+        else:
+            logger.info(f"Directory '{directory}' already exists.")
+
+cls = HuggingFaceModelDownloader()
