8bcb434ea2d1ee30d8185eff07982678667e023a
Louis Lee <louis@trypencil.com>
2023-09-28 23:48:05 +0800

    Codeformer mount

--- sample/docker.sh
+++ sample/docker.sh
@@ -13,6 +13,9 @@ CONTROLNET_EXTENSION_MODELS_PATH="/stable-diffusion-webui/extensions/sd-webui-co
 # Configure the below to point to local mountpoint
 MOUNT_CONTROLNET_EXTENSION_MODELS_PATH="/mnt/data/docker/sd-webui-controlnet-models"
 
+CODEFORMER_MODEL_PATH="/stable-diffusion-webui/repositories/CodeFormer/weights/"
+MOUNT_CODEFORMER_MODEL_PATH="/mnt/data/docker/sd-webui-codeformer-models"
+
 # Just in case there are pipelines outside our control that stubbornly downloads model using huggingface
 HF_HOME="/hf-home"  # this is already the default in the docker container but is also an ENV
 MOUNT_HF_HOME="/mnt/data/docker/sd-webui-hf-home"
@@ -20,6 +23,7 @@ MOUNT_HF_HOME="/mnt/data/docker/sd-webui-hf-home"
 # Make local mount dirs if they don't exist
 mkdir -p ${MOUNT_MAIN_MODELS_PATH}
 mkdir -p ${MOUNT_CONTROLNET_EXTENSION_MODELS_PATH}
+mkdir -p ${MOUNT_CODEFORMER_MODEL_PATH}
 
 # Run with network host, gpu, and mount points
 docker run --rm -t -d \
@@ -28,4 +32,5 @@ docker run --rm -t -d \
     -v ${MOUNT_HF_HOME}:${HF_HOME} \
     -v ${MOUNT_MAIN_MODELS_PATH}:${MAIN_MODELS_PATH} \
     -v ${MOUNT_CONTROLNET_EXTENSION_MODELS_PATH}:${CONTROLNET_EXTENSION_MODELS_PATH} \
+    -v ${MOUNT_CODEFORMER_MODEL_PATH}:${CODEFORMER_MODEL_PATH} \
     ${DOCKER_IMAGE_NAME}
