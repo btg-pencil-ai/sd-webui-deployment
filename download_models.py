482bf86ca6dc8e4021810a12757ac323324be5e1
Adithya-Shenoi <adithya.s@keyvalue.systems>
2023-09-28 09:36:31 +0530

    Updated the exact path

--- download_models.py
+++ download_models.py
@@ -8,7 +8,11 @@ MAIN_MODELS_PATH=os.environ.get("MAIN_MODELS_PATH",
 CONTROLNET_EXTENSION_MODELS_PATH=os.environ.get("CONTROLNET_EXTENSION_MODELS_PATH", 
                                                 "/stable-diffusion-webui/extensions/sd-webui-controlnet/models")
 
+
 logger = getLogger(__name__)
+
+STABLE_DIFFUSION_MODELS_PATH = os.path.join(MAIN_MODELS_PATH, "Stable-diffusion")
+VAE_MODELS_PATH = os.path.join(MAIN_MODELS_PATH, "VAE")
 
 class HuggingFaceModelDownloader():
     def __init__(self) -> None:
@@ -19,10 +23,10 @@ class HuggingFaceModelDownloader():
         os.makedirs(CONTROLNET_EXTENSION_MODELS_PATH, exist_ok=True)
 
         models_to_download = [
-            {'repo_id': 'stabilityai/stable-diffusion-xl-base-1.0', 'filename': 'sd_xl_base_1.0.safetensors', 'filepath': MAIN_MODELS_PATH},
-            {'repo_id': 'stabilityai/stable-diffusion-xl-refiner-1.0', 'filename': 'sd_xl_refiner_1.0.safetensors', 'filepath': MAIN_MODELS_PATH},
-            {'repo_id': 'stabilityai/sdxl-vae', 'filename': 'sdxl_vae.safetensors', 'filepath': MAIN_MODELS_PATH},
-            {'repo_id': 'stabilityai/sd-vae-ft-mse-original', 'filename': 'vae-ft-mse-840000-ema-pruned.ckpt', 'filepath': MAIN_MODELS_PATH},
+            {'repo_id': 'stabilityai/stable-diffusion-xl-base-1.0', 'filename': 'sd_xl_base_1.0.safetensors', 'filepath': STABLE_DIFFUSION_MODELS_PATH},
+            {'repo_id': 'stabilityai/stable-diffusion-xl-refiner-1.0', 'filename': 'sd_xl_refiner_1.0.safetensors', 'filepath': STABLE_DIFFUSION_MODELS_PATH},
+            {'repo_id': 'stabilityai/sdxl-vae', 'filename': 'sdxl_vae.safetensors', 'filepath': VAE_MODELS_PATH},
+            {'repo_id': 'stabilityai/sd-vae-ft-mse-original', 'filename': 'vae-ft-mse-840000-ema-pruned.ckpt', 'filepath': VAE_MODELS_PATH},
             {'repo_id': 'lllyasviel/sd_control_collection', 'filename': 'diffusers_xl_canny_full.safetensors', 'filepath': CONTROLNET_EXTENSION_MODELS_PATH},
             {'repo_id': 'lllyasviel/sd_control_collection', 'filename': 'sai_xl_depth_256lora.safetensors', 'filepath': CONTROLNET_EXTENSION_MODELS_PATH},
         ]
