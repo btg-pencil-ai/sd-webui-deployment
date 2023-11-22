499463b1653e9c60b6d9de06e5c8a9bfd91e5ae9
Louis Lee <louis@trypencil.com>
2023-11-22 18:29:32 +0800

    add status check before worker starts

--- src/config.py
+++ src/config.py
@@ -12,3 +12,11 @@ SD_WEBUI_API_ENDPOINT = os.environ.get("SD_WEBUI_API_ENDPOINT", "http://localhos
 BATCH_UPSCALE_ENDPOINT = "sdapi/v1/extra-batch-images/"
 SET_SD_WEBUI_OPTIONS_ENDPOINT = "sdapi/v1/options/"
 
+SERVER_CHECK_RETRIES=6
+SERVER_CHECK_BACKOFF=3  # factor * 2^num_prev_retries e.g. if 3, will be 3 * (2, 4, 8, ...)
+SERVER_CHECK_TIMEOUT=30
+
+SERVER_POST_RETRIES=3
+SERVER_POST_BACKOFF=1
+SERVER_POST_TIMEOUT=300
+
--- src/sd_webui_proxy/util.py
+++ src/sd_webui_proxy/util.py
@@ -1,6 +1,14 @@
 import io
 import base64
+import requests
+
+import src.config as config
+
 from PIL import Image
+from requests.adapters import HTTPAdapter
+from urllib.parse import urljoin
+from urllib3.util.retry import Retry
+
 
 def image_to_base64(image:Image):
     image_binary = io.BytesIO()
@@ -10,9 +18,35 @@ def image_to_base64(image:Image):
     base64_data = base64.b64encode(binary_data).decode('utf-8')
     return base64_data
 
+
 def base64_to_image(base64_image):
     binary_data = base64.b64decode(base64_image)
 
     # Convert binary data into a PIL Image object
     original_image = Image.open(io.BytesIO(binary_data))
     return original_image
+
+
+def get_session(retries: int, backoff_factor: int):
+    request_session = requests.Session()
+
+    retries = Retry(total=retries, backoff_factor=backoff_factor)
+    adapter = HTTPAdapter(max_retries=retries)
+    
+    request_session.mount('https://', adapter)
+    request_session.mount('http://', adapter)
+
+    return request_session
+
+
+def check_server_readiness():
+    session = get_session(config.SERVER_CHECK_RETRIES, config.SERVER_CHECK_BACKOFF)
+
+    res = session.get(url=urljoin(config.SD_WEBUI_API_ENDPOINT, "/sdapi/v1/progress"),
+                      timeout=config.SERVER_CHECK_TIMEOUT)
+    res.raise_for_status()
+
+    # Ready means job count is 0 in this dict structure
+    ready = res.json().get('state', {}).get('job_count', None) == 0
+
+    return ready
