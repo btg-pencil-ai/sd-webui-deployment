c799bb71a090010431c26c8c0be3d252cd5bbf74
Sandra <sandra@trypencil.com>
2024-01-02 16:50:10 +0530

    added changes

--- src/sd_webui_proxy/sdwebui_post_callback.py
+++ src/sd_webui_proxy/sdwebui_post_callback.py
@@ -1,8 +1,11 @@
-import src.config as config
 from urllib.parse import urljoin
 
+from src.aws.aws import s3_public_url
+import src.config as config
 from src.common import amqp
 from src.common.logger import get_logger
+from src.sd_webui_proxy.constant import GENERATED_IMAGES_S3_BASE_PATH
+from src.sd_webui_proxy.util import get_generated_image_s3_key, upload_base64_to_s3
 from src.sd_webui_proxy.sdwebui_post_callback_util import post_request, get_generated_images, get_upscaled_images
 
 logger = get_logger()
@@ -28,6 +31,9 @@ def sd_webui_post_callback_processor(params):
         width = callback_payload.get("gen_image_width")
         height = callback_payload.get("gen_image_height")
 
+        client_id = callback_payload.get("client_id")
+        batch_uuid = callback_payload.get("batch_uuid")
+
         if sd_webui_options_payload is not None:
             set_sd_webui_options_full_endpoint = urljoin(
                 config.SD_WEBUI_API_ENDPOINT, config.SET_SD_WEBUI_OPTIONS_ENDPOINT
@@ -41,10 +47,25 @@ def sd_webui_post_callback_processor(params):
             upscaled_and_resized_images = get_upscaled_images(upscale_payload=upscale_payload,resize_width=width,resize_height=height,result_images=result_images)
             result_images = list(filter(None, upscaled_and_resized_images))
 
-        callback_payload["base64_images"] = result_images
+        # convert to s3 urls
+        result_images_s3_urls = []
+        for result_image in result_images:
+            s3_key = get_generated_image_s3_key(
+                base_filename=GENERATED_IMAGES_S3_BASE_PATH,
+                client_id=client_id,
+                batch_uuid=batch_uuid,
+                image_ext="jpg",
+            )
+            upload_base64_to_s3(result_image, s3_key)
+            s3_url = s3_public_url(config.AWS_S3_BUCKET, s3_key)
+            logger.info(f"Generated image saved to {s3_url}")
+
+            result_images_s3_urls.append(s3_url)
+
+        callback_payload["result_images"] = result_images_s3_urls
 
     except Exception as e:
-        callback_payload["base64_images"] = None
+        callback_payload["result_images"] = None
         logger.error(e, exc_info=True)
 
     finally:
